9fde2493a3a5389f0337910f5251af6dd1ae73dd
VIKKRAM SM <129017402+Vikkram0821@users.noreply.github.com>
2024-10-30 23:03:45 +0530

    Update donorList_api.js

--- src/donorList_api.js
+++ src/donorList_api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/api/data";
+const API_URL = "https://blood-bank-backend-m709.onrender.com/api/data";
 
 export const getUserWithCity = async (city) => {
   const res = await axios.get(`${API_URL}/city/${city}`);
