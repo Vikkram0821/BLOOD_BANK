28d58dd3509df82904bebb497732ebab7af6be2b
VIKKRAM SM <129017402+Vikkram0821@users.noreply.github.com>
2024-11-01 11:22:51 +0530

    Update donorList.jsx

--- src/Components/donorList.jsx
+++ src/Components/donorList.jsx
@@ -29,7 +29,7 @@ const Donor = ({ deleteEffect, showTable, donors }) => {
   };
 
   return (
-    <div className={`donors_list_Table ${showTable ? "" : "hidden hide"}`}>
+    <div className={`donors_list_Table ${showTable ? "" : ""}`}>
       {donors.length > 0 ? (
         <>
           <table
